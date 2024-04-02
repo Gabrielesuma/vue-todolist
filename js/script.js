2b2ed34e6eac87ae4f7375778789cc86616e503a
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-02 16:11:52 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import {todo} from './data.js';
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            todo: todo
+        }
+    },
+    methods:{
+        
+    },
+    mounted(){
+        //console.log(this.todo);
+    }
+}).mount('#app');
